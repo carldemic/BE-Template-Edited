efdb999c9e147757afc23fc5c0afe5e4ee38d2d7
Computer <computer@Computers-MacBook-Air.local>
2021-03-28 15:01:42 +0300

    Fixed GET /contracts/:id to return a contract only if requested by a contractor or client listed in the contract

--- src/app.js
+++ src/app.js
@@ -2,20 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const { Op } = require("sequelize");
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
+/*
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const contractId = req.params.id
+    const profileId  = req.profile.id
+
+    const contractFilter = {
+    							where:{
+    								id: contractId,
+    								[Op.or]: [
+    									{
+    										ContractorId: profileId
+    									},
+    									{
+    										ClientId: profileId
+    									}
+    								]
+    							}
+    						}
+
+    const contract = await Contract.findOne(contractFilter)
+
+    // Return 404 to avoid enumeration, even if the contract exists under another client or contractor
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
 module.exports = app;
