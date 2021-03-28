973fa0461103ec81c096362fabbc9f5b09957b99
Carlo De Micheli <me@carlodemicheli.com>
2021-03-28 16:23:15 +0300

    Created GET Unpaid Jobs endpoint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
-const { Op } = require("sequelize");
+const {Op} = require("sequelize");
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -36,6 +36,67 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     // Return 404 to avoid enumeration, even if the contract exists under another client or contractor
     if(!contract) return res.status(404).end()
     res.json(contract)
+})
+
+app.get('/contracts',getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    const profileId  = req.profile.id
+
+    const contractsFilter = 
+    	{
+    		where:{
+    				[Op.not]: [{ status: "terminated"}],
+    				[Op.or]: [
+    					{
+    						ContractorId: profileId
+    					},
+    					{
+    						ClientId: profileId
+    					}
+    				]
+    			}
+    	}
+
+    const contracts = await Contract.findAll(contractsFilter)
+
+    if(!contracts) return res.status(404).end()
+    res.json(contracts)
+})
+
+app.get('/jobs/unpaid',getProfile ,async (req, res) =>{
+	const {Contract, Job} = req.app.get('models')
+    const profileId  = req.profile.id
+
+    const jobsFilter = 
+    	{
+    		where:{
+    				// In the database table Jobs, the column "paid" = NULL or 1
+    				// Be careful if setting it to zero to indicate "unpaid" as this would not work
+    				// This could also be paid != ,
+					paid: {[Op.eq]: null},
+			},
+    		include: [
+    			{
+    				model: Contract,
+    				where: {
+    					status: "in_progress",
+	    				[Op.or]: [
+    						{
+    							ContractorId: profileId
+    						},
+    						{
+    							ClientId: profileId
+    						}
+    					]
+    				}
+    			}
+    		]
+    	}
+
+    const unpaidJobs = await Job.findAll(jobsFilter)
+
+    if(!unpaidJobs) return res.status(404).end()
+    res.json(unpaidJobs)
 })
 
 module.exports = app;
